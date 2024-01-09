94456cf076b6aa253625efaea7bd0c813f2a12bc
tirtahakimpambudhi <awebthp@gmail.com>
2024-01-09 12:00:00 +0530

    feat(helper) :  modified helper function

--- helper/helper.py
+++ helper/helper.py
@@ -3,18 +3,14 @@ from werkzeug.utils import secure_filename
 import os
 import base64
 
+import base64
+def save_file_blob(photo_data, filename):
+    photo_data.save(os.path.join(Config.UPLOAD_PATH, filename))
 
-
-def save_file_blob(blob_data,filename) :
-        blob_data = blob_data.split(",")[1]
-        photo = base64.b64decode(blob_data)
-        filename = secure_filename(filename)
-        filepath = os.path.join(os.getcwd(),Config.UPLOAD_PATH, filename)
-        with open(filepath, 'wb') as f:
-            f.write(photo)
 def blob_to_base64(blob_data):
-        try : 
-                blob_data = blob_data.split(",")[1]
-                return base64.b64decode(blob_data)
-        except Exception as e:
-               raise e
+    try: 
+        blob_data = blob_data.getvalue().decode()  # Konversi dari BytesIO ke string
+        blob_data = blob_data.split(",")[1]  # Pisahkan data blob dari prefix "data:image/png;base64,"
+        return base64.b64decode(blob_data)
+    except Exception as e:
+        raise e

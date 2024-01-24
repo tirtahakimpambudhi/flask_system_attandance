001daf540cbb03940c404765a26df386a22c2803
tirtahakimpambudhi <awebthp@gmail.com>
2024-01-24 12:00:00 +0530

    feat(models) : change the name tables and raise error

--- models/students.py
+++ models/students.py
@@ -1,38 +1,46 @@
+
 import logging
 
 from supabase import Client
 from config.config import Config
 
-class Students():
+class SupabaseStudents():
     def __init__(self, pool :Client):
         self.db :Client = pool
     def get_all_students(self):
         try:
-            return self.db.table(Config.DATABASE["table"]).select("*").execute()
+            return self.db.table(Config.DATABASE["tables"][0]).select("*").execute()
         except Exception as error:
             logging.error(f"error execution query students raise : {type(error).__name__} - { error}")
-            return None
+            raise error
     def get_students_by_id(self,id :int):
         try:
-            return self.db.table(Config.DATABASE["table"]).select("*").eq("id",id).limit(1).execute()
+            return self.db.table(Config.DATABASE["tables"][0]).select("*").eq("id",id).limit(1).execute()
         except Exception as error:
             logging.error(f"error execution query students raise : {type(error).__name__} - { error}")
-            return None
+            raise error
+    def get_students_by_key(self,key :str):
+        try:
+            return self.db.table(Config.DATABASE["tables"][0]).select("*").like("name",f"%{key}%").like("major",f"%{key}%").execute()
+        except Exception as error:
+            logging.error(f"error execution query students raise : {type(error).__name__} - { error}")
+            raise error
     def insert_students(self,student :dict):
         try:
-            return self.db.table(Config.DATABASE["table"]).insert(student).execute()
+            return self.db.table(Config.DATABASE["tables"][0]).insert(student).execute()
         except Exception as error:
             logging.error(f"error execution query students raise : {type(error).__name__} - { error}")
-            return None
+            raise error
     def update_students_by_id(self,id :int,student :dict):
         try:
-            return self.db.table(Config.DATABASE["table"]).update(student).eq("id",id).execute()
+            return self.db.table(Config.DATABASE["tables"][0]).update(student).eq("id",id).execute()
         except Exception as error:
             logging.error(f"error execution query students raise : {type(error).__name__} - { error}")
-            return None
+            raise error
     def delete_students_by_id(self,id :int):
         try:
-            return self.db.table(Config.DATABASE["table"]).delete().eq("id",id).execute()
+            return self.db.table(Config.DATABASE["tables"][0]).delete().eq("id",id).execute()
         except Exception as error:
             logging.error(f"error execution query students raise : {type(error).__name__} - { error}")
-            return None
+            raise error
+

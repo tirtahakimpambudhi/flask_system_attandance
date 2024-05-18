199cafc9064985d4db344ebcd111d5a0754b5e33
tirtahakimpambudhi <awebthp@gmail.com>
2024-05-18 10:45:08 +0700

    feat(route) : implement form register validations

--- blueprints/auth_bp.py
+++ blueprints/auth_bp.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
-
+from flask import Blueprint,render_template
+from validations.forms import RegisterForm
 auth_views = Blueprint("auth", __name__)
 
 @auth_views.route("/register",methods=["GET"])
 def register():
-    return "Register"
+    form = RegisterForm()
+    return render_template("auth/register.html",form=form)
 
 @auth_views.route("/login",methods=["GET"])
 def login():

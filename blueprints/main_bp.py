12438a7f4ab0cba6aa8e6f3d27594d1ccf82380b
tirtahakimpambudhi <awebthp@gmail.com>
2024-05-21 12:00:00 +0530

    feat(route) : modified main routes

--- blueprints/main_bp.py
+++ blueprints/main_bp.py
@@ -1,11 +1,11 @@
-from flask import Blueprint,render_template
-
+from flask import Blueprint,render_template,url_for
+import json
 main_views = Blueprint("main",__name__)
+
+teams = []
+with open('teams.json') as f:
+    teams = json.load(f)
 
 @main_views.route("/",methods=["GET"])
 def index():
-    return render_template("main/index.html")
-
-@main_views.route("/about",methods=["GET"])
-def about():
-    return render_template("main/about.html")
+    return render_template("main/index.html",teams=teams)

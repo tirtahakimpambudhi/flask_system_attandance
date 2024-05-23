0310b428c91a2fdce40aedb09370bdca3003ce7f
tirtahakimpambudhi <awebthp@gmail.com>
2024-05-23 17:07:00 +0700

    feat(app) : implement students , absence blueprint or routes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config.config import Config
-from blueprints.auth_bp import auth_views
+# from blueprints.auth_bp import auth_views
 from blueprints.main_bp import main_views
+from blueprints.absence_bp import absence_route
+from blueprints.students_bp import students_route
 from logger import app_log
 
 app = Flask(__name__)
@@ -11,7 +13,8 @@ def create_app():
     Application factory function to create and configure the Flask app.
     """
     try:
-        app.register_blueprint(auth_views)
+        app.register_blueprint(students_route,url_prefix='/students')
+        app.register_blueprint(absence_route,url_prefix='/absence')
         app.register_blueprint(main_views)
         app.logger = app_log.AppLogging().init_app(app)
         app.config.from_object(Config())
